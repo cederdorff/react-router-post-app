5209a1a3ae21b0e1268ab11e9d47faaebb4122b8
RACE - Rasmus Cederdorff <rasmus@cederdorff.com>
2025-02-11 16:05:34 +0100

    Fix UserAvatar and UserCard components: provide default alt text for images

--- app/components/UserCard.tsx
+++ app/components/UserCard.tsx
@@ -3,7 +3,7 @@ import type { UserType } from "~/models/User";
 export default function UserCard({ user }: { user: UserType }) {
   return (
     <article className="post-card">
-      <img src={user.image} alt={user.name} />
+      <img src={user.image || "https://placehold.co/100"} alt={user.name || "User name"} />
       <h3>{user.name}</h3>
       <p>{user.title}</p>
     </article>
