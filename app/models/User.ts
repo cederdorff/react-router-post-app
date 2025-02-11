586fe23d4b41323de881c4c8f7b38115d23b6cff
RACE - Rasmus Cederdorff <rasmus@cederdorff.com>
2025-02-11 09:09:55 +0100

    Add user authentication: implement sign-in and sign-up routes, hash passwords, and manage sessions

--- app/models/User.ts
+++ app/models/User.ts
@@ -1,4 +1,5 @@
 import { Schema, Types, model, type InferSchemaType } from "mongoose";
+import bcrypt from "bcrypt";
 
 // Define the schema for the User collection in MongoDB
 const userSchema = new Schema(
@@ -17,14 +18,33 @@ const userSchema = new Schema(
       required: true
     },
     title: String,
-    educations: [String]
+    educations: [String],
+    password: {
+      type: String,
+      required: true, // Ensure user passwords are required
+      select: false // Automatically exclude from query results
+    }
   },
   { timestamps: true } // Automatically add `createdAt` and `updatedAt` timestamps to each document
 );
 
+// pre save password hook
+userSchema.pre("save", async function (next) {
+  const user = this; // this refers to the user document
+
+  // only hash the password if it has been modified (or is new)
+  if (!user.isModified("password")) {
+    return next(); // continue
+  }
+
+  const salt = await bcrypt.genSalt(10); // generate a salt
+  user.password = await bcrypt.hash(user.password, salt); // hash the password
+  next(); // continue
+});
+
 // Infer TypeScript type for the schema
 // `InferSchemaType<typeof userSchema>` generates the type based on the schema definition
-export type UserType = InferSchemaType<typeof userSchema> & { _id: Types.ObjectId | string };
+export type UserType = InferSchemaType<typeof userSchema> & { _id: Types.ObjectId };
 
 // Create a Mongoose model for the User schema
 // This model provides an interface to interact with the "User" collection in MongoDB
--- app/services/auth.server.ts
+++ app/services/auth.server.ts
@@ -1,36 +1,36 @@
-import { Authenticator, AuthorizationError } from "remix-auth";
-import { sessionStorage } from "./session.server";
-import { FormStrategy } from "remix-auth-form";
 import bcrypt from "bcrypt";
 import mongoose from "mongoose";
+import { Authenticator } from "remix-auth";
+import { FormStrategy } from "remix-auth-form";
 import type { UserType } from "~/models/User";
+import User from "~/models/User";
 
 // Create an instance of the authenticator, pass a generic with what
 // strategies will return and will store in the session
-export let authenticator = new Authenticator<UserType>(sessionStorage, {
-  sessionErrorKey: "sessionErrorKey" // keep in sync
-});
+export let authenticator = new Authenticator<UserType>();
 
 // Tell the Authenticator to use the form strategy
 authenticator.use(
   new FormStrategy(async ({ form }) => {
-    let mail = form.get("mail");
-    let password = form.get("password");
+    const mail = form.get("mail");
+    const password = form.get("password");
 
     // do some validation, errors are saved in the sessionErrorKey
     if (!mail || typeof mail !== "string" || !mail.trim()) {
-      throw new AuthorizationError("Email is required and must be a string");
+      throw new Error("Email is required and must be a string");
+      // throw new AuthorizationError("Email is required and must be a string");
     }
 
     if (!password || typeof password !== "string" || !password.trim()) {
-      throw new AuthorizationError("Password is required and must be a string");
+      throw new Error("Password is required and must be a string");
+      // throw new AuthorizationError("Password is required and must be a string");
     }
 
     // verify the user
-    const user = await verifyUser({ mail, password });
+    const user = await verifyUser(mail, password);
     if (!user) {
       // if problem with user throw error AuthorizationError
-      throw new AuthorizationError("User not found");
+      // throw new AuthorizationError("User not found");
     }
     console.log(user);
     return user;
@@ -38,17 +38,19 @@ authenticator.use(
   "user-pass"
 );
 
-async function verifyUser({ mail, password }) {
-  const user = await mongoose.models.User.findOne({ mail }).select("+password");
+async function verifyUser(mail: string, password: string) {
+  const user = await User.findOne({ mail }).select("+password");
   if (!user) {
-    throw new AuthorizationError("No user found with this email.");
+    throw new Error("No user found with this email.");
+    // throw new AuthorizationError("No user found with this email.");
   }
 
   const passwordMatch = await bcrypt.compare(password, user.password);
   if (!passwordMatch) {
-    throw new AuthorizationError("Invalid password.");
+    throw new Error("Invalid password.");
+    // throw new AuthorizationError("Invalid password.");
   }
   // Remove the password from the user object before returning it
-  user.password = undefined;
+  // delete user.password;
   return user;
 }
