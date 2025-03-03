8db26f411254135e31f7d921799cd7bd97a5a5f2
RACE - Rasmus Cederdorff <rasmus@cederdorff.com>
2025-03-03 09:08:03 +0100

    Remove leading slashes from route paths for consistency in route definitions

--- app/routes.ts
+++ app/routes.ts
@@ -6,12 +6,12 @@ export default [
     route("posts/:id", "routes/post-detail.tsx"),
     route("posts/:id/update", "routes/post-update.tsx"),
     route("add-post", "routes/post-add.tsx"),
-    route("/users", "routes/users.tsx"),
-    route("/profile", "routes/profile.tsx")
+    route("users", "routes/users.tsx"),
+    route("profile", "routes/profile.tsx")
   ]),
   route("posts/:id/destroy", "routes/post-destroy.ts"),
-  route("/signin", "routes/signin.tsx"),
-  route("/auth/github/callback", "routes/auth/github-callback.ts"),
-  route("/auth/github", "routes/auth/github.ts"),
-  route("/auth/signout", "routes/auth/signout.ts")
+  route("signin", "routes/signin.tsx"),
+  route("auth/github/callback", "routes/auth/github-callback.ts"),
+  route("auth/github", "routes/auth/github.ts"),
+  route("auth/signout", "routes/auth/signout.ts")
 ] satisfies RouteConfig;
