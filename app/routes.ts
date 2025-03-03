e433866b38f171f7f1904fecc462a28471183f59
RACE - Rasmus Cederdorff <rasmus@cederdorff.com>
2025-03-03 09:08:47 +0100

    Fix users route path by removing leading slash for correct routing

--- app/routes.ts
+++ app/routes.ts
@@ -6,5 +6,5 @@ export default [
   route("posts/:id/update", "routes/post-update.tsx"),
   route("posts/:id/destroy", "routes/post-destroy.tsx"),
   route("add-post", "routes/post-add.tsx"),
-  route("/users", "routes/users.tsx")
+  route("users", "routes/users.tsx")
 ] satisfies RouteConfig;
