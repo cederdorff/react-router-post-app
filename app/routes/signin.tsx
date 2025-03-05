c98dedc6d678d591825956507de78294a2caca0a
RACE - Rasmus Cederdorff <rasmus@cederdorff.com>
2025-03-05 21:20:38 +0100

    Update sign-in page text and change OAuth form container class for clarity

--- app/routes/signin.tsx
+++ app/routes/signin.tsx
@@ -13,8 +13,8 @@ export default function SignIn() {
   return (
     <div id="sign-in-page" className="page">
       <h1>Sign In</h1>
-      <div className="signin-form-container">
-        <p>Sign in with your GitHub or Google account.</p>
+      <p>Or sign in with your GitHub or Google account.</p>
+      <div className="oauth-form-container">
         <Form method="post" action="/auth/github">
           <div className="btns full">
             <button>Sign In with GitHub</button>
